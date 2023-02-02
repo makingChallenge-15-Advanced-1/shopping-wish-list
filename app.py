14079ebe10862b41aae928da2ee83cd091cec445
sayhong0321 <124114271+sayhong0321@users.noreply.github.com>
2023-02-02 11:01:31 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 from pymongo import MongoClient
 import certifi
 ca = certifi.where()
-client = MongoClient('mongodb+srv://sayhong_db:happy*721@cluster0.cnaox23.mongodb.net/?retryWrites=true&w=majority', 27017, tlsCAFile=ca)
+client = MongoClient('db 정보', 27017, tlsCAFile=ca)
 db = client.dbsparta
 
 @app.route('/')
@@ -15,9 +15,9 @@ def home():
    return render_template('index.html')
 
 #모든 db 정보 list를 return
-@app.route("/shop?list=All", methods=["GET"])                                          
-def item_get():                               #url에서 image 추출해서 전달하기
-    url = "url"
+@app.route("/wishlist?list=All", methods=["GET"])                                          
+def wishlist_get():                               #url에서 image 추출해서 전달하기
+    url = "받아온 url"
     image = image_from_url(url) 
     return jsonify({'msg':'접속 성공'})
 
@@ -26,30 +26,30 @@ def image_from_url(url):
     return "이것이 이미지"
 
 #db에 새로운 정보를 추가
-@app.route("/shop", methods=["POST"])
-def item_post():                              #받는 변수 : url, name, price, memo, status
+@app.route("/wishlist", methods=["POST"])
+def wishlist_post():                              #받는 변수 : url, name, price, memo, status
     url_receive = request.form['url_give']    #listId 부여하는 식 넣기
     image = image_from_url(url_receive)       #db 저장 : url, name, price, memo, status, listId
     return jsonify({'msg':'저장 완료!'})
 
 #db의 특정 정보를 수정
-@app.route("/shop/listId", methods=["PUT"])    #받는 변수 : url, name, price, memo, status, listId
-def item_modify():
+@app.route("/wishlist/listId", methods=["PUT"])    #받는 변수 : url, name, price, memo, status, listId
+def wishlist_modify():
     url_receive = request.form['url_give']      #db 저장 : url, name, price, memo, status, listId
     image = image_from_url(url_receive)
     return jsonify({'msg':'수정 완료!'})
 
 #해당 번호의 db 정보를 return
-@app.route("/shop/listId", methods=["GET"])        
-def item_listId_get():                             #url에서 image 추출해서 전달하기
+@app.route("/wishlist/listId", methods=["GET"])        
+def wishlist_listId_get():                             #url에서 image 추출해서 전달하기
     listId_receive = request.args.get('listId')
     url = "url"
     image = image_from_url(url)
     return jsonify({'msg':'전달 완료!'})
 
 #해당 번호의 db 정보를 삭제
-@app.route("/shop/listId", methods=["DELETE"])
-def meta_del():
+@app.route("/wishlist/listId", methods=["DELETE"])
+def wishlist_del():
     listId_receive = request.form['listId_give']
     return jsonify({'msg':'삭제 완료!'})
 
