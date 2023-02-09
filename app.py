eb4d0a0c936594ee5cbeb97128fb408f84e646eb
sayhong0321 <window0321@naver.com>
2023-02-09 14:07:46 +0900

    flask_bcrypt, flask_jwt_extended를 설치하세요. login 기능 추가

--- app.py
+++ app.py
@@ -4,21 +4,107 @@ app = Flask(__name__)
 import requests
 from bs4 import BeautifulSoup
 
+# login을 위한 모듈
+from flask_bcrypt import Bcrypt
+from datetime import datetime, timedelta, timezone 
+from flask_jwt_extended import *
+
+#mongo db library
 from pymongo import MongoClient
 import certifi
 ca = certifi.where()
+client = MongoClient('mongodb+srv://test:sparta@Cluster0.kwrmlin.mongodb.net/?retryWrites=true&w=majority', 27017, tlsCAFile=ca)
+db = client.dbsparta
 
 # 몽고DB TypeError
 # TypeError: Object of type ObjectId is not JSON serializable
 from bson.json_util import dumps
 
-client = MongoClient('mongodb+srv://test:sparta@Cluster0.kwrmlin.mongodb.net/?retryWrites=true&w=majority', 27017, tlsCAFile=ca)
-db = client.dbsparta
+# JWT 매니저 활성화
+app.config.update(DEBUG = True, JWT_SECRET_KEY = "spartateam" )
+# If true this will only allow the cookies that contain your JWTs to be sent
+# over https. In production, this should always be set to True
+app.config["JWT_COOKIE_SECURE"] = False     #Configure application to store JWTs in cookies
+app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
+app.config['JWT_ACCESS_COOKIE_PATH'] = '/' # access cookie를 보관할 url (Frontend 기준)
+app.config['JWT_REFRESH_COOKIE_PATH'] = '/' # refresh cookie를 보관할 url (Frontend 기준)
 
-@app.route('/')
-def home():
+jwt = JWTManager(app)
+
+#route 설정
+@app.route('/')    #첫화면
+def home():     
+    return render_template('main.html')
+@app.route('/index')    #wishlist page
+def index():
     return render_template('index.html')
+@app.route('/register')    #회원가입 page
+def open_register_page():
+    return render_template('register.html')
 
+############################################
+#               로그인 API                 #
+############################################
+
+#회원 등록
+@app.route("/user/register", methods=["POST"])      #register.html의 register()에서 call
+def register():
+    # 회원정보 생성
+    user_id = request.form['user_id_give']
+    user_pwd = request.form['user_pwd_give']
+    pwd_hash = bcrypt.generate_password_hash(user_pwd)   # 비밀번호 암호화
+    doc = {
+        'user_id' : user_id,
+        'user_pwd' : pwd_hash
+    }
+    db.users.insert_one(doc)        #db에 회원정보 등록
+    return jsonify({'msg':'등록 완료'})
+
+# 동일한 user_id를 가진 회원이 없는지 확인
+@app.route("/user/user_id_check", methods=["GET"])      #register.html의 check_user_id()에서 call
+def register_check_id():
+    user_id = request.args.get('user_id')
+    check_id = db.users.find_one({'user_id':user_id})
+    if check_id is not None:
+        return jsonify({'result' : "fail"})
+    return jsonify({'result' : "success"})
+
+#로그인하기
+@app.route("/user/login", methods=["POST"])   #main.html의 login() 에서 call
+def login():
+    # 회원정보 생성
+    user_id = request.form['user_id_give']
+    user_pwd = request.form['user_pwd_give']
+    # 회원등록하기와 마찬가지로 암호화 
+    pwd_hash = bcrypt.generate_password_hash(user_pwd)
+
+    user = db.users.find_one({'user_id': user_id}, {'user_pwd': pwd_hash})
+    if user is None:
+        return jsonify({'msg': '아이디나 비밀번호가 잘못되었습니다!!', 'login': False})
+
+    # Create the tokens we will be sending back to the user
+    access_token = create_access_token(identity=user_id, expires_delta=False)
+    refresh_token = create_refresh_token(identity=user_id)
+
+    response = jsonify({'msg':'로그인 성공', 'login': True})
+    set_access_cookies(response, access_token)
+    set_refresh_cookies(response, refresh_token)
+    print(access_token)
+    print(refresh_token)
+    return response, 200
+
+# 로그아웃
+@app.route('/token/remove', methods=['POST'])           #index.html의 logout()에서 call
+def logout():
+    response = jsonify({'msg':'로그아웃 성공', 'logout': True})
+    unset_jwt_cookies(response)     #delete the cookies in order to logout
+    return response, 200
+
+
+
+############################################
+#               위시리스트 API             #
+############################################
 
 #url에서 image 추출하는 함수
 def image_from_url(url_receive):
@@ -37,26 +123,27 @@ def image_from_url(url_receive):
     return image
 
 #GET API 
-#조건에 맞는 list를 return -> 아직 해결 안됨.(16:50)
+#조건에 맞는 list를 return
 @app.route("/wishlist", methods=["GET"])
+@jwt_required(optional=True)
 def wishlist_get():
+    current_user_id = get_jwt()['sub']
+    print(current_user_id)
     list_method = request.args.get('list')
     if list_method == 'all':
-        wishlist = list(db.wishlist.find({'status': 'toBuy'}, {'_id': False}))
-        wishlist = wishlist + list(db.wishlist.find({'status': 'hold'}, {'_id': False}))
-        wishlist = wishlist + list(db.wishlist.find({'status': 'order'}, {'_id': False}))
-    elif list_method == 'toBuy':
-        wishlist = list(db.wishlist.find({'status': 'toBuy'}, {'_id': False}))
-    elif list_method == 'hold':
-        wishlist = list(db.wishlist.find({'status': 'hold'}, {'_id': False}))
-    elif list_method == 'order':
-        wishlist = list(db.wishlist.find({'status': 'order'}, {'_id': False}))
+        wishlist = list(db.wishlist.find({}, {'_id': False}))
+    elif list_method == 'ready':
+        wishlist = list(db.wishlist.find({'status': 'ready'}, {'_id': False}))
+    elif list_method == 'refer':
+        wishlist = list(db.wishlist.find({'status': 'refer'}, {'_id': False}))
+    elif list_method == 'done':
+        wishlist = list(db.wishlist.find({'status': 'done'}, {'_id': False}))
 
     for item in wishlist:
         url = item['url']
         image = image_from_url(url) 
         item['image'] = image
-    return jsonify({'wishlist':wishlist})
+    return jsonify({'wishlist':wishlist, 'user_id':current_user_id})
 
 #해당 번호의 db 정보를 return
 @app.route("/wishlist/{listId}", methods=["GET"])
@@ -66,7 +153,7 @@ def wishlist_listId_get():
     url = listId_item['url']
     image = image_from_url(url)
     listId_item['image'] = image
-    return jsonify({'listId_item': dumps(listId_item)}) # TypeError 해결용
+    return jsonify({'listId_item':listId_item})
 
 
 #POST API
@@ -127,6 +214,35 @@ def wishlist_modify():
     }
     db.wishlist.update_one({'listId':int(listId_receive)},{'$set':doc})
     return jsonify({'msg':'수정 완료!'})
+
+# GET, POST사용해서 수정
+# @app.route("/wishlist/{listId}/status", methods=["GET", "POST"])
+# def wishlist_modify():
+#     if request.method == "GET":
+#         listId_receive = request.args.get('listId_give')
+#         listId_item = db.wishlist.find_one({'listId': int(listId_receive)})
+#         url = listId_item['url']
+#         image = image_from_url(url)
+#         listId_item['image'] = image
+#         return jsonify({'listId_item':listId_item})
+#     else:
+#         url_receive = request.form['url_give']          
+#         name_receive = request.form['name_give']        
+#         price_receive = request.form['price_give']
+#         memo_receive = request.form['memo_give']
+#         status_receive = request.form['status_give']
+#         listId_receive = request.form['listId_give']
+
+#         doc = {                                 #db 저장 : url, name, price, memo, status, listId
+#             'url': url_receive,
+#             'name' : name_receive,
+#             'price' : price_receive,
+#             'memo' : memo_receive,
+#             'status' : status_receive,
+#             'listId' : int(listId_receive)
+#         }
+#         db.wishlist.update_one({'listId':int(listId_receive)},{'$set':doc})
+#         return jsonify({'msg':'수정 완료!'})
 
 #listId에 해당하는 db의 status만 수정
 @app.route("/wishlist/{listId}/status", methods=["PUT"])    #받는 변수 : url, name, price, memo, status, listId
